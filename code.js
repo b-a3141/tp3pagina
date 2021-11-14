9b41155cde6bbfcfb9c39362bb09095793c12773
CTMagnus <braianboianelli3@gmail.com>
2021-11-14 09:47:12 -0300

    Primer commit

---
+++ code.js
@@ -0,0 +1,9 @@
+let switchBtn = document.querySelector(".switch")
+
+document.addEventListener("DOMContentLoaded",()=>{
+    promp("cargo")
+})
+
+switchBtn.addEventListener("click", ()=>{
+   prompt("gun")
+})
